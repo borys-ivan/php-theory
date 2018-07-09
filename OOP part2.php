7ccf26dfd35a967c96bd193a04b0f81cfc32366d
Ivan <borys.ivan@gmail.com>
2018-07-09 12:17:57 +0300

    Update OOP part2.php

--- OOP part2.php
+++ OOP part2.php
@@ -120,3 +120,125 @@ echo $result1->getCourse();
 
 ?>
 
+
+
+Сделайте класс Driver (Водитель), который будет наследоваться от класса Worker из предыдущей задачи. 
+Этот метод должен вносить следующие private поля: водительский стаж, категория вождения (A, B, C).
+
+Class Worker
+{
+
+    protected $name;
+    protected $age;
+    protected $salary;
+
+
+
+     function __construct($name, $age, $salary)
+    {
+
+        $this->name = $name;
+        $this->age = $age;
+        $this->salary = $salary;
+    }
+
+
+
+    public function getName()
+    {
+
+        return $this->name;
+
+    }
+
+
+    public function getSalary()
+    {
+
+        return $this->salary;
+
+    }
+
+    public function getAge()
+    {
+
+        return $this->age;
+
+    }
+
+
+    public function setSalary($salary)
+    {
+
+        $this->salary=$salary;
+
+    }
+
+    public function setAge($age)
+    {
+
+        $this->age=$age;
+
+    }
+
+}
+
+
+Class Driver extends Worker{
+
+    private $sta;
+    private $cat;
+
+    function  __construct($name,$age,$salary,$sta,$cat)
+    {
+
+parent::__construct($name,$age,$salary);
+
+
+        $this->sta = $sta;
+        $this->cat = $cat;
+    }
+
+    public function getSta()
+    {
+
+        return $this->sta;
+
+    }
+
+    public function getCat()
+    {
+
+        return $this->cat;
+
+    }
+
+
+    public function setSta($sta)
+    {
+
+        $this->sta=$sta;
+
+    }
+
+    public function setCat($cat)
+    {
+
+        $this->cat=$cat;
+
+    }
+
+
+}
+
+
+
+$result1=new Driver("ivan",25,2000,"A",2);
+
+echo $result1->getName();
+echo $result1->getAge();
+
+echo $result1->getSta();
+echo $result1->getCat();
+
+?>
